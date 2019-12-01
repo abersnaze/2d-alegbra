f1a2c437675b80eedc0d7e4d6ad6ec02768b0e8f
George Campbell <gscampbell@netflix.com>
2019-12-01 01:52:58 -0800

    v0.3.0

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,15 @@
-import { expression } from "../src";
+import 'mocha';
+import { expect } from 'chai';
+import expression from '../src/';
 
 test("1+2 => 3", () => {
   const q = expression(1).plus(2);
-  expect(q.toString()).toBe("3");
+  expect(q.toString()).to.equal("3");
 });
 
 test("(1+x)+2 => x+3", () => {
   const q = expression(1).plus(Symbol("x")).plus(2);
-  expect(q.toString()).toBe(`+
+  expect(q.toString()).to.equal(`+
 ├ 3
 └ x`);
 });
@@ -15,7 +17,7 @@ test("(1+x)+2 => x+3", () => {
 test("1+(x+(0+2)) => x+3", () => {
   const x = Symbol("x");
   const q = expression(1).push(x).push(0).plus(2).plus().plus();
-  expect(q.toString()).toBe(`+
+  expect(q.toString()).to.equal(`+
 ├ 3
 └ x`);
 });
@@ -23,7 +25,7 @@ test("1+(x+(0+2)) => x+3", () => {
 test("(x+y)+(1+z) => (1+(x+(y+z)", () => {
   const [x, y, z] = [Symbol("x"), Symbol("y"), Symbol("z")];
   const q = expression(x).plus(y).push(1).plus(z).plus();
-  expect(q.toString()).toBe(`+
+  expect(q.toString()).to.equal(`+
 ├ 1
 └ +
   ├ z
@@ -31,32 +33,32 @@ test("(x+y)+(1+z) => (1+(x+(y+z)", () => {
     ├ y
     └ x`);
   const dq = q.derivative(x);
-  expect(dq.toString()).toBe("1");
+  expect(dq.toString()).to.equal("1");
 });
 
 test("x+0 => x", () => {
   const [x] = [Symbol("x")];
   const q = expression(x).plus(0);
-  expect(q.toString()).toBe("x");
+  expect(q.toString()).to.equal("x");
 });
 
 test("x+x => 2*x", () => {
   const x = Symbol("x");
   const q = expression(x).plus(x);
-  expect(q.toString()).toBe(`*
+  expect(q.toString()).to.equal(`*
 ├ 2
 └ x`);
 });
 
 test("2*3 => 6", () => {
   const q = expression(2).times(3);
-  expect(q.toString()).toBe("6");
+  expect(q.toString()).to.equal("6");
 });
 
 test("(2+x)*3 => 6+3*x", () => {
   const x = Symbol("x");
   const q = expression(2).plus(x).times(3);
-  expect(q.toString()).toBe(`+
+  expect(q.toString()).to.equal(`+
 ├ 6
 └ *
   ├ 3
@@ -66,7 +68,7 @@ test("(2+x)*3 => 6+3*x", () => {
 test("(2*x)*2 => 4*x", () => {
   const x = Symbol("x");
   const q = expression(2).times(x).times(2);
-  expect(q.toString()).toBe(`*
+  expect(q.toString()).to.equal(`*
 ├ 4
 └ x`);
 });
@@ -74,7 +76,7 @@ test("(2*x)*2 => 4*x", () => {
 test("2*(x*2) => 4*x", () => {
   const x = Symbol("x");
   const q = expression(2).push(x).times(2).times();
-  expect(q.toString()).toBe(`*
+  expect(q.toString()).to.equal(`*
 ├ 4
 └ x`);
 });
@@ -82,7 +84,7 @@ test("2*(x*2) => 4*x", () => {
 test("(2*x)*(3*x) => 4*x", () => {
   const x = Symbol("x");
   const q = expression(2).times(x).push(3).times(x).times();
-  expect(q.toString()).toBe(`*
+  expect(q.toString()).to.equal(`*
 ├ 6
 └ ^
   ├ x
@@ -92,25 +94,25 @@ test("(2*x)*(3*x) => 4*x", () => {
 test("x*0 => 0", () => {
   const x = Symbol("x");
   const q = expression(x).times(0);
-  expect(q.toString()).toBe("0");
+  expect(q.toString()).to.equal("0");
 });
 
 test("x*1 => x", () => {
   const x = Symbol("x");
   const q = expression(x).times(1);
-  expect(q.toString()).toBe("x");
+  expect(q.toString()).to.equal("x");
   const dq = q.derivative(x);
-  expect(dq.toString()).toBe("1");
+  expect(dq.toString()).to.equal("1");
 });
 
 test("x*x => x^2", () => {
   const x = Symbol("x");
   const q = expression(x).times(x);
-  expect(q.toString()).toBe(`^
+  expect(q.toString()).to.equal(`^
 ├ x
 └ 2`);
   const dq = q.derivative(x);
-  expect(dq.toString()).toBe(`*
+  expect(dq.toString()).to.equal(`*
 ├ 2
 └ x`);
 });
@@ -118,7 +120,7 @@ test("x*x => x^2", () => {
 test("2+(3*x) => x", () => {
   const x = Symbol("x");
   const q = expression(2).push(3).times(x).plus();
-  expect(q.toString()).toBe(`+
+  expect(q.toString()).to.equal(`+
 ├ 2
 └ *
   ├ 3
@@ -130,7 +132,7 @@ test("derivative", () => {
   const y = Symbol("y");
 
   const q = expression(x).minus(3).squared();
-  expect(q.toString()).toBe(`+
+  expect(q.toString()).to.equal(`+
 ├ 9
 └ +
   ├ *
@@ -140,7 +142,7 @@ test("derivative", () => {
     ├ x
     └ 2`);
   const dx = q.derivative(x);
-  expect(dx.toString()).toBe(`+
+  expect(dx.toString()).to.equal(`+
 ├ -6
 └ *
   ├ 2
---
+++ test/tree.test.ts
@@ -0,0 +1,46 @@
+import 'mocha';
+import { expect } from 'chai';
+import { add, degreeComparator, mult, pow, variable } from "../src/tree";
+
+test("cmp unnamed", () => {
+    const [a, b, c] = [variable(), variable(), variable()];
+
+    expect(degreeComparator(a, b)).to.equal(1);
+    expect(degreeComparator(b, c)).to.equal(1);
+    expect(degreeComparator(a, c)).to.equal(1);
+    expect(degreeComparator(c, b)).to.equal(-1);
+    expect(degreeComparator(b, a)).to.equal(-1);
+    expect(degreeComparator(c, a)).to.equal(-1);
+    expect(degreeComparator(c, c)).to.equal(0);
+    expect(degreeComparator(b, b)).to.equal(0);
+    expect(degreeComparator(a, a)).to.equal(0);
+});
+
+test("cmp named", () => {
+    const [a, b, c] = [variable(Symbol("a")), variable(Symbol("b")), variable(Symbol("c"))];
+
+    expect(degreeComparator(a, b)).to.equal(1);
+    expect(degreeComparator(b, c)).to.equal(1);
+    expect(degreeComparator(a, c)).to.equal(1);
+    expect(degreeComparator(c, b)).to.equal(-1);
+    expect(degreeComparator(b, a)).to.equal(-1);
+    expect(degreeComparator(c, a)).to.equal(-1);
+    expect(degreeComparator(c, c)).to.equal(0);
+    expect(degreeComparator(b, b)).to.equal(0);
+    expect(degreeComparator(a, a)).to.equal(0);
+});
+
+test("cmp mult", () => {
+    const [a, b] = [variable(Symbol("a")), variable(Symbol("b"))];
+
+    expect(degreeComparator(pow(a, 2), mult(a, a))).to.equal(0);
+    expect(degreeComparator(mult(a, b), mult(b, a))).to.equal(0);
+});
+
+test("cmp add", () => {
+    const [a, b] = [variable(Symbol("a")), variable(Symbol("b"))];
+
+    expect(degreeComparator(mult(a, b), add(a, b))).to.equal(1);
+    expect(degreeComparator(add(a, b), add(a, b))).to.equal(-1);
+    expect(degreeComparator(add(a, b), add(a, b))).to.equal(-1);
+});
