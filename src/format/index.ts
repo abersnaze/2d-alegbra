cfbc89973b043624d931e028c8620612f188a335
George Campbell <gscampbell@netflix.com>
2023-02-26 14:40:29 -0800

    clear

--- src/format/index.ts
+++
@@ -1,6 +0,0 @@
-import { INode } from "../node";
-
-export interface Format {
-  binary(indent: string, op: string, a: INode, b: INode | string): string;
-  func(indent: string, func: string, arg: INode): string;
-}
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Expression } from "./Expression";
-import { toNode, Term } from "./node";
-
-export function expression(a: Term): Expression {
-  return new Expression(toNode(a));
-}
-export * from "./Expression";
-export * from "./ExpressionStack";
-export { Term } from "./node";
